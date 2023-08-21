c2004b1ac62d96b19439c4ebba2b7195a1b5f6a2
Moreni26 <ayomide2605@gmail.com>
2023-08-21 09:32:49 -0700

    updated

---
+++ aux_lists2.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * add_rvar_node - adds variables at the end
+ * of  r_var lists.
+ * @head: heads of the linked lists.
+ * @lvar: lengths of the variables.
+ * @val: value of the variables.
+ * @lval: lengths of the value.
+ * Return: address of the heads.
+ */
+r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
+{
+	r_var *new, *temp;
+
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
+
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_rvar_list - frees a r_var lists
+ * @head: head of the linked lists.
+ * Return: null
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
